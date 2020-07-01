d059e834f5fc93ab39faf6c364624784fa9ddc0a
Lens92 <67616150+Lens92@users.noreply.github.com>
2020-07-01 18:13:46 +1200

    Update SalesVisualiser.java

--- FileProcessing/SalesVisualiser.java
+++ FileProcessing/SalesVisualiser.java
@@ -3,9 +3,9 @@
 // You may not distribute it in any other way without permission.
 
 /* Code for COMP102 - 2020T1, Assignment 4
- * Name: Shaylen Mamidi
- * Username: mamidishay 
- * ID: 300535776
+ * Name: 
+ * Username: 
+ * ID: 
  */
 
 
